a05dd56537cca448da70ee1faad8d8539be62cfa
Maria <123@gmail.com>
2019-11-12 13:16:04 +0300

    code refactoring

--- lab4/src/main/java/ru/mirea/primes/ProbablePrime.java
+++ lab4/src/main/java/ru/mirea/primes/ProbablePrime.java
@@ -5,61 +5,76 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Logger;
 
+/**
+ * Class for making probable primes with set bitLength.
+ * Algorithm of making:
+ * * Make random {@code BigInteger} with bitLength
+ * * Check division by small primes up to {@code SMALL_PRIMES_MAX}
+ * * Perform check by Rabin Miller tests {@code RABIN_MILLER_TESTS_NUM} times
+ * * If checks fail generate another candidate
+ *
+ * @see BigInteger
+ */
 public class ProbablePrime {
 
-    private static final int RM_TESTS_NUM = 5;
+    private static final int RABIN_MILLER_TESTS_NUM = 5;
     private static final int SMALL_PRIMES_MAX = 2000;
-
+    // for debugging
     private static final Logger logger = Logger.getLogger(ProbablePrime.class.getName());
-    private BigInteger number;
-    private int bits;
-    private ArrayList<Integer> smallPrimes = generateArraySmall();
+    private BigInteger probablePrime;
+    private int bitLength;
+    private ArrayList<Integer> smallPrimes = generateSmallPrimes();
 
-    /**
-     * Generate probable prime number with known bits length
-     *
-     * @param bits number of bits in ProbablePrime, > 1
-     * @return generated prime
-     */
-    public static ProbablePrime generate(int bits) {
-        return new ProbablePrime(bits);
+
+    private ProbablePrime(int bitLength) {
+        this.bitLength = bitLength;
+        do {
+            probablePrime = rabinMillerTests(checkPrimesDiv(modify(generateRandom(bitLength))));
+        } while (probablePrime == null);
+        logger.info("PROBABLE PRIME = " + probablePrime);
     }
 
+
     /**
-     * Get value of prime
-     * @return prime as BigInteger
+     * Generate probable prime with chosen bitLength
+     *
+     * @param bitLength length of prime, > 0
+     * @return generated number
+     */
+    public static ProbablePrime generate(int bitLength) {
+        return new ProbablePrime(bitLength);
+    }
+
+
+    /**
+     * Get prime value
+     * @return prime as BigInteger, > 0
      */
     public BigInteger value() {
-        return number;
+        return probablePrime;
     }
+
 
     /**
-     * Get bit length of prime
-     * @return integer value of bit length
+     * Get bitLength of prime
+     * @return bitLength
      */
     public int bitLength() {
-        return bits;
+        return bitLength;
     }
 
 
-    private ProbablePrime(int bits) {
-        this.bits = bits;
-        do {
-            number = rabinMillerTests(checkDiv(modify(generateRandom(bits))));
-        } while (number == null);
-        logger.info("PROBABLE PRIME = " + number);
+    /* Прогоняем алгоритм эратосфена на числах от 2 до SMALL_PRIMES_MAX */
+    private ArrayList<Integer> generateSmallPrimes() {
+        return filterNonPrimes(generateSequence(2, SMALL_PRIMES_MAX));
     }
 
-    /* Генерируем массив из 2000 простых с помощью решета Эратосфена */
-    private ArrayList<Integer> generateArraySmall() {
-        return filterArraySmall(mkNumArray(2, SMALL_PRIMES_MAX));
-    }
 
-    private ArrayList<Integer> filterArraySmall(ArrayList<Integer> sequence) { // CHECK
+    private ArrayList<Integer> filterNonPrimes(ArrayList<Integer> sequence) {
         int currIndex = 0;
         Integer current = sequence.get(currIndex);
         Integer maxElem = SMALL_PRIMES_MAX;
-
+        // delete numbers: number % current == 0
         while (current * current < maxElem) {
             Integer toDel = current * current;
             while (toDel < maxElem) {
@@ -74,7 +89,7 @@ public class ProbablePrime {
         return sequence;
     }
 
-    private ArrayList<Integer> mkNumArray(int startNum, int finishNum) {
+    private ArrayList<Integer> generateSequence(int startNum, int finishNum) {
         ArrayList<Integer> array = new ArrayList<>();
         for (int i = startNum; i < finishNum; i++) {
             array.add(i);
@@ -82,16 +97,17 @@ public class ProbablePrime {
         return array;
     }
 
-    // If passes test returns number, else null
+    // If passes test returns probablePrime, else null
     private BigInteger rabinMillerTests(BigInteger candidate) {
         if (candidate == null) return null;
         if (candidate.compareTo(BigInteger.valueOf(2)) <= 0) return candidate;
 
+        // count s and d: (pow(2,s)*d) == (candidate - 1)
         int s = countS(candidate);
         BigInteger d = countD(candidate, s);
         logger.info(candidate + " s " + s + " d " + d);
 
-        for (int i = 0; i < RM_TESTS_NUM; i++) {
+        for (int i = 0; i < RABIN_MILLER_TESTS_NUM; i++) {
             if (!rabinMillerTest(candidate, s, d, countN(candidate)))
                 return null;
         }
@@ -108,7 +124,7 @@ public class ProbablePrime {
         if (x.equals(BigInteger.ONE) || x.equals(n.subtract(BigInteger.ONE)))
             return true;
         for (int i = 0; i < s - 1; i++) {
-            x = x.modPow(BigInteger.valueOf(2),n);
+            x = x.modPow(BigInteger.valueOf(2), n);
             if (x.equals(BigInteger.ONE)) return false;
             if (x.equals(n.subtract(BigInteger.ONE))) return true;
         }
@@ -119,7 +135,7 @@ public class ProbablePrime {
         int s = 0;
         for (BigInteger num = candidate.subtract(BigInteger.ONE);
              num.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO);
-             num = num.divide(BigInteger.valueOf(2))){
+             num = num.divide(BigInteger.valueOf(2))) {
             s++;
         }
         return s;
@@ -127,28 +143,24 @@ public class ProbablePrime {
 
     private BigInteger countD(BigInteger candidate, int s) {
         return candidate.subtract(BigInteger.ONE).divide(
-                BigInteger.valueOf((long)Math.pow(2, s)));
+                BigInteger.valueOf((long) Math.pow(2, s)));
     }
 
-    // If passes test returns number, else null
-    private BigInteger checkDiv(BigInteger number) {
-        boolean isDiv = false;
+    // If passes test returns probablePrime, else null
+    private BigInteger checkPrimesDiv(BigInteger number) {
         for (Integer prime : smallPrimes) {
-            if (BigInteger.valueOf(prime).compareTo(number) > 0) break;
-            if (BigInteger.valueOf(prime).equals(number)) continue;
-            if (number.mod(BigInteger.valueOf(prime)).equals(BigInteger.ZERO)) {
-                isDiv = true;
-            }
+            if (BigInteger.valueOf(prime).compareTo(number) >= 0) return number;
+            if (number.mod(BigInteger.valueOf(prime)).equals(BigInteger.ZERO)) return null;
         }
-        return isDiv ? null : number;
+        return number;
     }
 
     /*
      * Increase probability of being prime.
-     * First and last bit of number are set to 1.
+     * First and last bit of probablePrime are set to 1.
      */
     private BigInteger modify(BigInteger number) {
-        return number.setBit(0).setBit(bits - 1);
+        return number.setBit(0).setBit(bitLength - 1);
     }
 
     private BigInteger generateRandom(int bits) {
