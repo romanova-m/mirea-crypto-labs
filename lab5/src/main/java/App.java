f8c2bddcc3dd56c40550d6beaa3cbd0622c81001
Maria <123@gmail.com>
2019-11-12 13:31:52 +0300

    add lab5

---
+++ lab5/src/main/java/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+        int[] array = {6,2,1,2,8,10};
+        sort(array, 1, 10);
+    }
+
+    private static void sort(int[] array, int minVal, int maxVal) {
+        int[] temp = new int[maxVal - minVal + 1];
+        for (int elem: array) {
+            temp[elem - minVal]++;
+        }
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[i]; j++) {
+                System.out.print(i + minVal + " ");
+            }
+        }
+    }
+}
